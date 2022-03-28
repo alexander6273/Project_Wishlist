f9cb466b02683b734ffddb4d68960471b1bcc7b3
alexander6273 <alex6273@stud.kea.dk>
2022-03-28 13:25:29 +0200

    initial commit

---
+++ src/test/java/com/projectwishlist/ProjectWishlistApplicationTests.java
@@ -0,0 +1,15 @@
+package com.projectwishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWishlistApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}

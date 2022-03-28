a46eb45d69c4ddb0b90f3bd69134b1b3256a8bbd
OliverAndreasen <oliv2058@stud.kea.dk>
2022-03-28 14:05:58 +0200

    DatabaseConnection test added

--- src/main/java/com/projectwishlist/controllers/MainController.java
+++ src/main/java/com/projectwishlist/controllers/MainController.java
@@ -1,5 +1,28 @@
 package com.projectwishlist.controllers;
 
+import com.projectwishlist.services.DatabaseServices;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+
+@RestController
 public class MainController
 {
+    DatabaseServices databaseServices = new DatabaseServices();
+    //Controller methods
+    @GetMapping("/")
+    public String index() {
+        System.out.println("LOL");
+        return "fejl";
+    }
+    @GetMapping("/lol")
+    public String lol(){
+        try {
+            return databaseServices.getAllItems();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "Hej med dig";
+    }
 }
--- src/main/java/com/projectwishlist/services/DatabaseServices.java
+++ src/main/java/com/projectwishlist/services/DatabaseServices.java
@@ -1,5 +1,52 @@
 package com.projectwishlist.services;
 
-public class DatabaseServices
-{
+import com.projectwishlist.models.Item;
+import com.projectwishlist.repositories.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class DatabaseServices {
+    private Connection connection = null;
+
+    public DatabaseServices() {
+        this.connection = DatabaseConnection.getConnection();
+    }
+
+    private Statement getStatement() {
+        try {
+            return connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ResultSet getResultSet(String sql) {
+        try {
+            return Objects.requireNonNull(getStatement()).executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getAllItems() throws SQLException {
+        //ArrayList<Item> users = new ArrayList<>();
+
+        ResultSet resultSet = getResultSet("SELECT * FROM Item");
+        String itemName = "fejl";
+        System.out.println(itemName);
+
+        while (resultSet.next()) {
+            itemName = resultSet.getString("item_name");
+            System.out.println(itemName);
+        }
+        return itemName;
+        // return users;
+    }
 }

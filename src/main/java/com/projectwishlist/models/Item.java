5df710b1a2d1f41d4f5b1e28c6787a2fc77908dc
alex6273 <alex6273@stud.kea.dk>
2022-03-29 10:46:28 +0200

    item model

--- src/main/java/com/projectwishlist/models/Item.java
+++ src/main/java/com/projectwishlist/models/Item.java
@@ -2,4 +2,52 @@ package com.projectwishlist.models;
 
 public class Item
 {
+    private int id;
+    private String name;
+    private int price;
+    private String link;
+
+    public Item(int id, String name, int price, String link) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.link = link;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    @Override
+    public String toString() {
+        return "item id: " + id + "item name: " + name + "item price: " + price + "item link: " + link;
+    }
 }

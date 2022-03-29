46e8ec935b23e41670428d4b08c086bbfa3152ef
kris52h6 <kris52h6@stud.kea.dk>
2022-03-29 10:49:16 +0200

    Wishlist model

--- src/main/java/com/projectwishlist/models/Wishlist.java
+++ src/main/java/com/projectwishlist/models/Wishlist.java
@@ -1,5 +1,42 @@
 package com.projectwishlist.models;
 
-public class Wishlist
-{
+public class Wishlist {
+    private int id;
+    private String name;
+    private String link;
+    private String userId;
+
+    public Wishlist(int id, String name, String link, String userId) {
+        this.id = id;
+        this.name = name;
+        this.link = link;
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Wishlist{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", link='" + link + '\'' +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
 }
